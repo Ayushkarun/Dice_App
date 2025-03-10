8339ddb1bdc766b51fd294cc94bf308ccc91e24a
Ayushkarun <ayushkarun2580@gmail.com>
2025-03-10 16:56:47 +0530

    Added text and app bar for details

--- lib/Work/dice.dart
+++ lib/Work/dice.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
+
 ///import 'package:dice_icons/dice_icons.dart';
 
 class Dice extends StatelessWidget {
@@ -9,7 +10,11 @@ class Dice extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(backgroundColor: Colors.white),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text('Dice Roller', style: TextStyle(color: Colors.black)),
+        centerTitle: true,
+      ),
       body: Dicepage(),
     );
   }
@@ -23,34 +28,35 @@ class Dicepage extends StatefulWidget {
 }
 
 class _DicepageState extends State<Dicepage> {
-     var dicenumber=1;
- @override
+  var dicenumber = 1;
+  @override
   Widget build(BuildContext context) {
- 
     return Center(
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          Text(
+            'Click the Dice!',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
           Container(
             height: 150.0,
             width: 150.0,
-            child:TextButton(
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.green
-              ),
-              onPressed: (){
-                setState(() {               
-                   print('$dicenumber button pressed');
-                dicenumber=Random().nextInt(6)+1;
-                  
+            child: TextButton(
+              style: TextButton.styleFrom(foregroundColor: Colors.green),
+              onPressed: () {
+                setState(() {
+                  print('$dicenumber button pressed');
+                  dicenumber = Random().nextInt(6) + 1;
                 });
+              },
+              child: Image.asset('Asset/Dice$dicenumber.png'),
+            ),
 
-              }, 
-            child: Image.asset('Asset/Dice$dicenumber.png')),///string intepretation
+            ///string intepretation
           ),
         ],
       ),
     );
   }
 }
-
